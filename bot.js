7110009176566f83782f16d85bcc7dd914a6ddc8
nisshii0313 <yuta19990313@gmail.com>
2019-04-24 12:57:02 +0900

    add fetch section

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 'use strict';
+require('dotenv').config();
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
+//const fetch = require('node-fetch');
+//const axios = require('axios');
 
 // create LINE SDK config from env variables
 const config = {
@@ -25,23 +26,40 @@ app.post('/callback', line.middleware(config), (req, res) => {
   .all(req.body.events.map(handleEvent))
   .then((result) => res.json(result))
   .catch((err) => {
-	  console.error(err);
-	  res.status(500).end();
+    console.error(err);
+    res.status(500).end();
   });
 });
 
 // event handler
 async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-	  // ignore non-text-message event
-	  return Promise.resolve(null);
+    // ignore non-text-message event
+    return Promise.resolve(null);
   }
 
-  const item = encodeURIComponent(event.message.text);
-  const contents = await axios.get(`http://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${item}&source=ja&target=en`);
-  console.log(contents.data);
+  const message = event.message.text;
+  //console.log(message);
+
+  // translation API
+  // const item = encodeURIComponent(event.message.text);
+  // const contents = await axios.get(`http://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${item}&source=ja&target=en`);
+  // console.log(contents.data);
+
+  // const item = encodeURIComponent(event.message.text);
+
+  // fetch(`http://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${item}&source=ja&target=en`)
+  // .then(response => response.text())
+  // .then(data => {
+  //   console.log(data) // Prints result from `response.json()` in getRequest
+  // })
+  // .catch(error => console.error(error))
+
   // create a echoing text message
-  const echo = { type: 'text', text: contents.data };
+  const echo = { type: 'text', text: message };
+
+  // create a echoing text message by using translation API
+  //const echo = { type: 'text', text: contents.data };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
@@ -50,5 +68,5 @@ async function handleEvent(event) {
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`listening on ${port}`);
+  console.log(`listening on ${port}`);
 });
