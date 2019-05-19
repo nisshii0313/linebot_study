310330ac0b13bf9ab27aff73361cc13ac958ce28
nisshii0313 <yuta19990313@gmail.com>
2019-05-19 16:55:36 +0900

    wip: auto reply

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
-'use strict';
 require('dotenv').config();
 
 const line = require('@line/bot-sdk');
 const express = require('express');
 //const fetch = require('node-fetch');
-//const axios = require('axios');
+const axios = require('axios');
 
 // create LINE SDK config from env variables
 const config = {
@@ -39,30 +38,46 @@ async function handleEvent(event) {
   }
 
   const message = event.message.text;
-  //console.log(message);
+  console.log(message);
 
-  // translation API
-  // const item = encodeURIComponent(event.message.text);
-  // const contents = await axios.get(`http://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${item}&source=ja&target=en`);
-  // console.log(contents.data);
+  //translation API
+  const item = encodeURIComponent(event.message.text);
+  const contents = await axios.get(`http://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${item}&source=ja&target=en`).catch(e => console.error(e));0;
+  
 
-  // const item = encodeURIComponent(event.message.text);
+  let params = new URLSearchParams();
+  params.append("apikey", process.env.API_KEY_REPLY);
+  params.append("query", message);
+
+  const contents2 = await axios.post('https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk', params).catch(e => console.error(e));;
+  console.log(contents2.data.results[0].reply);
+
+  //const item = encodeURIComponent(event.message.text);
 
   // fetch(`http://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${item}&source=ja&target=en`)
   // .then(response => response.text())
   // .then(data => {
   //   console.log(data) // Prints result from `response.json()` in getRequest
+  //   const echo = { type: 'text', text: data };
+  //   return client.replyMessage(event.replyToken, echo);
   // })
   // .catch(error => console.error(error))
 
   // create a echoing text message
-  const echo = { type: 'text', text: message };
+  //const echo = { type: 'text', text: message };
 
   // create a echoing text message by using translation API
-  //const echo = { type: 'text', text: contents.data };
+  const echo = { type: 'text', text: contents2.data.results[0].reply };
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  client.replyMessage(event.replyToken, echo);
+
+  const contents3 = await axios.get(`https://chatbot-api.userlocal.jp/api/chat?message=${encodeURIComponent(contents2.data.results[0].reply)}&key=${process.env.API_KEY_REPLY2}`).catch(e => console.error(e));
+  console.log(contents3.data.result);
+  const echo2 = { type: 'text', text: contents2.data.result[0].reply};
+
+  client.replyMessage(event.replyToken, echo2);
+
 }
 
 // listen on port
